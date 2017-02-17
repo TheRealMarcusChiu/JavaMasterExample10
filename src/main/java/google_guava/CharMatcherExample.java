d6f65fe820128ff2f80a98902357db8523ab9a37
Marcus Chiu <marcuschiu9@gmail.com>
2017-02-17 15:00:37 -0600

    added guava's CharMatcher example added java 8's Optional example

---
+++ src/main/java/google_guava/CharMatcherExample.java
@@ -0,0 +1,19 @@
+package google_guava;
+
+import com.google.common.base.CharMatcher;
+import org.junit.Test;
+
+public class CharMatcherExample {
+
+    @Test
+    public void test() {
+
+        String s = "asdfghjkl";
+
+        if(CharMatcher.ASCII.matchesAllOf(s)) {
+            System.out.println("matches");
+        } else {
+            System.out.println("not matches");
+        }
+    }
+}
