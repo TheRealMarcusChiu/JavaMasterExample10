63bf83026bdb3bb561cb274ac4ed35c4cbc24a7c
Marcus Chiu <marcuschiu9@gmail.com>
2017-05-05 15:17:52 -0500

    updated Google guava Preconditions example

--- src/main/java/google_guava/PreconditionsExample.java
+++ src/main/java/google_guava/PreconditionsExample.java
@@ -7,17 +7,24 @@ public class PreconditionsExample {
 
     public void greet(String greeting) throws IllegalArgumentException {
         Preconditions.checkArgument(greeting != null, "Null greeting");
+        Preconditions.checkArgument(greeting.equals("Hello, World!"), "not a string");
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void failure() {
+    public void failure1() {
         PreconditionsExample preconditionsExample = new PreconditionsExample();
         preconditionsExample.greet(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void failure2() {
+        PreconditionsExample preconditionsExample = new PreconditionsExample();
+        preconditionsExample.greet("Bugs Bunny");
     }
 
     @Test
     public void success() {
         PreconditionsExample preconditionsExample = new PreconditionsExample();
-        preconditionsExample.greet("hello");
+        preconditionsExample.greet("Hello, World!");
     }
 }
